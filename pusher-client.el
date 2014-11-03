5ddfdfbf180eecf6f3d640a7cb60d863c992ffeb
Phil Newton <phil@sodaware.net>
2014-11-02 19:06:02 -0500

    Added Cask file and initial skeleton

---
+++ pusher-client.el
@@ -0,0 +1,40 @@
+;;; pusher-client.el --- Library for working with pusher.com API's
+
+;; Copyright (C) 2014 Phil Newton
+
+;; Author: Phil Newton <phil@sodaware.net>
+;; Version: 0.1.0
+;; Package-Requires: ((json "1.2"))
+;; Keywords: pusher api websockets
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;; Dependencies
+
+(require 'json)
+(require 'url-http)
+
+(defvar url-http-end-of-headers)
+
+;; Configuration
+
+
+(provide 'pusher-client)
+;;; pusher-client.el ends here
